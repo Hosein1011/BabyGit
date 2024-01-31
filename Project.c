c9e7ef7e0381548089176a0b151cb02de01d1317
Hossein <hosein.hka@gmail.com>
2024-01-31 13:56:51 +0330

    add fucking git add to code

--- Project.c
+++ Project.c
@@ -6,7 +6,8 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-
+#define True 1
+#define Zero 0
 char *CheckInit(const char *path) {
     DIR *dir = opendir(path);
     struct dirent *entry;
@@ -19,7 +20,23 @@ char *CheckInit(const char *path) {
     closedir(dir);
     return NULL;
 }
-
+void CurrentWorkingDirectory(char *cwd)
+{
+    char *cwd;      //in voide check beshe
+    char buffer[4096];
+    cwd = getcwd(buffer , 4096);
+    return cwd;
+}
+char * FindCWD( char * Data)
+{
+    char *fcwd = (char *)malloc(4096);
+    char buffer[4096];
+    char * path = (char *)malloc(4096);
+    path = CheckInit(fcwd);
+    fcwd = fcwd + strlen(path);////////oyyyyyyyyy
+    sprintf(fcwd , "%s%s" , fcwd , Data);//
+    return fcwd ;
+}
 void CreatConfig(int IsGlobal, char *mode, char *Data) {
     if (IsGlobal) {
         char path[] = "/mnt/e/ffproject/"; // path chi bashe ?
@@ -74,11 +91,11 @@ void CreatConfig(int IsGlobal, char *mode, char *Data) {
         }
     }
 }
-void GitInit ()
+void GitInit (int mode , char *Data)
 {
     char *cwd;
     char buffer[4096];
-    getcwd (buffer , 4096);
+    cwd = getcwd (buffer , 4096);
 if (CheckInit(cwd) != NULL)
 {
     puts("This is already a  babygit repository :)");
@@ -86,8 +103,106 @@ if (CheckInit(cwd) != NULL)
 }
 else
 {
+    mkdir(".babygit" , "0777");
+    //mkdir(".babygit/branch", 0777);
+    //mkdir(".babygit/branch/master", 0777);
+    //mkdir(".babygit/commits", 0777);
+    //mkdir(".babygit/stage", 0777);
+    //fopen(".babygit/status.txt", "w");
+    //fopen(".babygit/branch/master/reset.txt", "w");
+    return;
+}
+}
+void GitAdd(int mode , char *Data)
+{
+    if (mode == -1) puts("This file or directory is invalid"); return;
+    char *cwd ;
+    char buffer[4096];
+    cwd = getcwd (buffer , 4096);
+    char *Repository = CheckInit (cwd);
+    char *PathStats = (char *)malloc(4096);
+    sprintf(PathStats , "%s/.babygit/status.txt" , Repository); //status.txt?why
+    if (mode == Zero) //masir nadarim bayad ijad she
+    {
+        char *NewPath = (char *)malloc(4096);
+        NewPath = FindCWD(Data);
+        if (NewPath[0] = "/") NewPath += 1;
+        chdir("/"); chdir(Repository); //chdir(".babygit/stage")
+        mkdir(NewPath , 0777); chdir(cwd);
+        struct dirent *fp ;
+        char *path = (char *)malloc(4096);
+        path = FindCWD(Data);
+        DIR *direction = opendir(Data);
+        if (direction == NULL) puts("directory not found :("); return;
+        while ((fp = readdir(direction)) != NULL)
+        {
+                if (strcmp(fp->d_name , ".") == 0 ||strcmp(fp->d_name , "..") == 0)
+                {
+                    continue;
+                }
+                if (fp->d_type == DT_DIR)
+                {
+                    char path[4096];
+                    sprintf(path , "%s/%s" , cwd , Data);
+                    chdir("/");
+                chdir(path);
+                add(Zero, fp->d_name);
+                chdir(cwd);
+                }
+                else if (fp->d_type == DT_REG)
+            {
+                char path[4096];
+                sprintf(path, "%s/%s", cwd, Data);
+                chdir("/");
+                chdir(path);
+                add(True, fp->d_name); //check beshe
+                chdir(cwd);
+            }
+            else
+            {
+                printf("%s pleas enter a valid type :(\n", fp->d_name); //chera puts nis?
+            }
+        }
+                char abs_path[4096];
+        sprintf(abs_path, "%s/%s", cwd, Data);
+        FILE *status = fopen(PathStats, "a"); //txt?
+        fprintf(status, "%s\n", abs_path);
+        fclose(status);
+        return;
+    }
+else
+{
+    char FileNAme[4096];
+    sprintf(FileNAme , "%s%s" , cwd , Data);
+    FILE *file = fopen(FileNAme , "r");
+    if ( file = NULL ) puts("can not finde the file"); return;
+    char *path =(char *)malloc(4096);
+    path = FindCWD(Data);
+    char *Copydest = (char *)malloc(4096);
+    char PathStats[4096];
+    char PathABS[4096];
+    sprintf(PathABS , "%s%s" , cwd , Data);
+    sprintf(PathStats , "%s/.babygit/status.txt" , Repository);
+    FILE *stats = fopen(PathStats , "a");
+    fprintf(stats , "%s/n" , PathABS);
+    sprintf(Copydest , "%s/.babygit/stage%s" , Repository , path);
+    //copyfile(FileNAme , Copydest);
+    Copydest = Copydest + 1 ;
+    FILE *sourcefile = fopen("FileNAme.txt" , "rb");
+    if (sourcefile == NULL) puts("right now your file does not exist :("); return -1; //or -1
+    chdir("/"); chdir("copydest");
+    FILE *Copydest = fopen("Copydest" ,  "wb");
+    char buffer[1024]; size_t size;
+    while ((size = fread(buffer , 1 , size , FileNAme)) > 0 )
+    {
+        fwrite(buffer , 1 , size , Copydest);
+    }
+    fclose(Copydest);
+    fclose(file);
+    fclose(stats);
 
 }
+
 }
 
 int main(int argc, char *argv[]) {  // Fixed the syntax error here
